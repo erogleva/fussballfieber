3944618877ce711547fc6f16663a45ceea7c4047
Thomas Handle <thomas.handle@gmail.com>
2019-01-30 20:06:25 +0100

    update

--- server.ts
+++ server.ts
@@ -1,103 +1,13 @@
-/*import 'zone.js/dist/zone-node';
+import 'zone.js/dist/zone-node';
 import 'reflect-metadata';
-
-const test = process.env['TEST'] === 'true';
-
-
-const files = fs.readdirSync(`${process.cwd()}/dist/app/server`);
-
-
-import { enableProdMode } from '@angular/core';
-import * as express from 'express';
-import * as compression from 'compression';
-import * as cookieparser from 'cookie-parser';
-const { provideModuleMap } = require('@nguniversal/module-map-ngfactory-loader');
-
-const mainFiles = files.filter((file) => file.startsWith('main'));
-const hash = mainFiles[0].split('.')[1];
-const { AppServerModuleNgFactory, LAZY_MODULE_MAP } = require(`./dist/app/server/main.${hash}`);
-import { ngExpressEngine } from '@nguniversal/express-engine';
-import { REQUEST, RESPONSE } from '@nguniversal/express-engine/tokens';
-const PORT = process.env.PORT || 4000;
-import { ROUTES } from './static.paths';
-import { exit } from 'process';
-
-enableProdMode();
-
-const app = express();
-app.use(compression());
-app.use(cookieparser());
-
-const redirectowww = false;
-const redirectohttps = false;
-const wwwredirecto = false;
-
-app.use((req, res, next) => {
-  // for domain/index.html
-  if (req.url === '/index.html') {
-    res.redirect(301, 'https://' + req.hostname);
-  }
-
-  // check if it is a secure (https) request
-  // if not redirect to the equivalent https url
-  if (
-    redirectohttps &&
-    req.headers['x-forwarded-proto'] !== 'https' &&
-    req.hostname !== 'localhost'
-  ) {
-    // special for robots.txt
-    if (req.url === '/robots.txt') {
-      next();
-      return;
-    }
-    res.redirect(301, 'https://' + req.hostname + req.url);
-  }
-
-  // www or not
-  if (redirectowww && !req.hostname.startsWith('www.')) {
-    res.redirect(301, 'https://www.' + req.hostname + req.url);
-  }
-
-  // www or not
-  if (wwwredirecto && req.hostname.startsWith('www.')) {
-    const host = req.hostname.slice(4, req.hostname.length);
-    res.redirect(301, 'https://' + host + req.url);
-  }
-
-  // for test
-  if (test && req.url === '/test/exit') {
-    res.send('exit');
-    exit(0);
-    return;
-  }
-
-  next();
-});
-
-
-app.set('view engine', 'html');
-app.set('views', 'src');
-
-app.get('*.*', express.static(path.join(__dirname, '.', 'dist')));
-app.get(ROUTES, express.static(path.join(__dirname, '.', 'static')));
-
-app.get('*', (req, res) => {
-  global['navigator'] = req['headers']['user-agent'];
-  const http =
-    req.headers['x-forwarded-proto'] === undefined ? 'http' : req.headers['x-forwarded-proto'];
-
-  const url = req.originalUrl;
-  );
-});
- *
-
 
 const domino = require('domino');
 const fs = require('fs');
-const template = fs.readFileSync('dist/app/browser/index.html').toString();
+const path = require('path');
+const template = fs.readFileSync(path.join(process.cwd(), 'dist/app/browser', 'index.html')).toString();
 const win = domino.createWindow(template);
-global['window'] = win;
 
+global['window'] = win;
 Object.defineProperty(win.document.body.style, 'transform', {
   value: () => {
     return {
@@ -108,84 +18,11 @@ Object.defineProperty(win.document.body.style, 'transform', {
 });
 global['document'] = win.document;
 global['CSS'] = null;
-// global['XMLHttpRequest'] = require('xmlhttprequest').XMLHttpRequest;
 global['Prism'] = null;
-
-import 'zone.js/dist/zone-node';
-import { enableProdMode } from '@angular/core';
-
-// Express Engine
-import { ngExpressEngine } from '@nguniversal/express-engine';
-import { REQUEST, RESPONSE } from '@nguniversal/express-engine/tokens';
-// Import module map for lazy loading
-import { provideModuleMap } from '@nguniversal/module-map-ngfactory-loader';
-
-import * as express from 'express';
-import { join } from 'path';
-
-// Faster server renders w/ Prod mode (dev mode never needed)
-enableProdMode();
-
-// Express server
-const app = express();
-
-const PORT = process.env.PORT || 4000;
-const DIST_FOLDER = join(process.cwd(), 'dist');
-
-// * NOTE :: leave this as require() since this file is built Dynamically from webpack
-const { AppServerModuleNgFactory, LAZY_MODULE_MAP } = require('./dist/app/server/main');
-
-// Our Universal express-engine (found @ https://github.com/angular/universal/tree/master/modules/express-engine)
-app.engine('html', ngExpressEngine({
-  bootstrap: AppServerModuleNgFactory,
-  providers: [
-    provideModuleMap(LAZY_MODULE_MAP)
-  ]
-}));
-
-app.set('view engine', 'html');
-app.set('views', join(DIST_FOLDER, 'app/browser'));
-
-
-// Server static files from /browser
-app.get('*.*', express.static(join(DIST_FOLDER, 'browser'), {
-  maxAge: '1y'
-}));
-
-// All regular routes use the Universal engine
-app.get('*', (req, res) => {
-  res.render('index', {
-    req: req,
-    res: res,
-    /*providers: [
-      {
-        provide: REQUEST,
-        useValue: req,
-      },
-      {
-        provide: RESPONSE,
-        useValue: res,
-      },
-      {
-        provide: 'ORIGIN_URL',
-        useValue: `//${req.headers.host}`,
-      },
-    ],
-  }, (err, html) => {
-    res.status(html ? 200 : 500).send(html || err.message);
-  });
-});
-
-// Start up the Node server
-app.listen(PORT, () => {
-  console.log(`Node Express server listening on http://localhost:${PORT}`);
-}); */
 
 (global as any).WebSocket = require('ws');
 (global as any).XMLHttpRequest = require('xhr2');
 
-import 'zone.js/dist/zone-node';
-import 'reflect-metadata';
 import { enableProdMode } from '@angular/core';
 // Express Engine
 import { ngExpressEngine } from '@nguniversal/express-engine';
@@ -194,6 +31,7 @@ import { provideModuleMap } from '@nguniversal/module-map-ngfactory-loader';
 
 import * as express from 'express';
 import { join } from 'path';
+import { REQUEST, RESPONSE } from '@nguniversal/express-engine/tokens';
 
 // Faster server renders w/ Prod mode (dev mode never needed)
 enableProdMode();
@@ -228,7 +66,20 @@ app.get('*.*', express.static(DIST_FOLDER, {
 
 // All regular routes use the Universal engine
 app.get('*', (req, res) => {
-  res.render('index', { req });
+  res.render('index', {
+    req: req,
+    res: res,
+    /* providers: [
+      {
+        provide: REQUEST,
+        useValue: req,
+      },
+      {
+        provide: RESPONSE,
+        useValue: res,
+      },
+    ], */
+  });
 });
 
 // Start up the Node server
